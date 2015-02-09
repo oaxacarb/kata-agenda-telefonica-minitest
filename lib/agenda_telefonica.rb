edbf2f9b45845ebe4518ccd784e001f3e80a9a3e
Carlos García <carlos.garcia@logicalbricks.com>
2015-02-09 10:46:28 -0600

    Solución a la que llegamos en el coding dojo del día 8 de febrero, trabajando en pares Carlos y Donet.

--- lib/agenda_telefonica.rb
+++ lib/agenda_telefonica.rb
@@ -1,2 +1,29 @@
 class AgendaTelefonica
+  def validar(lista)
+    return true if lista.size == 1
+    evalua_lista(lista)
+  end
+
+  private
+
+  def subfijo?(subfijo, numero)
+    numero.index(subfijo) == 0
+  end
+
+  def evalua_lista(lista)
+    posicion_numero_actual = 0
+    while posicion_numero_actual < lista.size
+      posicion_numero_a_comparar = 0
+      while posicion_numero_a_comparar < lista.size
+        if posicion_numero_a_comparar != posicion_numero_actual
+          if lista[posicion_numero_actual].size <= lista[posicion_numero_a_comparar].size
+            return false if subfijo?(lista[posicion_numero_actual], lista[posicion_numero_a_comparar])
+          end
+        end #if
+        posicion_numero_a_comparar += 1
+      end #while
+      posicion_numero_actual += 1
+    end #while
+    true
+  end #def 
 end
--- test/agenda_telefonica_test.rb
+++ test/agenda_telefonica_test.rb
@@ -3,4 +3,50 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class AgendaTelefonicaTest < MiniTest::Unit::TestCase
+  def test_true
+    verdadero=true
+    assert_equal true, verdadero
+  end
+
+  def test_lista_con_un_elemento_es_valida
+    lista = ['2334']
+    telefono = AgendaTelefonica.new
+    resultado = telefono.validar(lista)
+    assert_equal true, resultado
+  end
+
+  def test_lista_con_dos_elementos_un_prefijo_no_es_valida
+    lista = ['2334', '233']
+    telefono = AgendaTelefonica.new
+    resultado = telefono.validar(lista)
+    assert_equal false, resultado
+  end
+
+  def test_lista2_con_dos_elementos_un_prefijo_no_es_valida
+    lista = ['233', '2334']
+    telefono = AgendaTelefonica.new
+    resultado = telefono.validar(lista)
+    assert_equal false, resultado
+  end
+
+  def test_lista3_con_dos_elementos_un_prefijo_no_es_valida
+    lista = ['23', '2334']
+    telefono = AgendaTelefonica.new
+    resultado = telefono.validar(lista)
+    assert_equal false, resultado
+  end
+
+  def test_lista_con_tres_elementos_un_prefijo_no_es_valida
+    lista = ['2334','4321', '23']
+    telefono = AgendaTelefonica.new
+    resultado = telefono.validar(lista)
+    assert_equal false, resultado
+  end
+
+  def test_lista_con_dos_elementos_iguales_no_es_valida
+    lista = ['2334', '2334']
+    telefono = AgendaTelefonica.new
+    resultado = telefono.validar(lista)
+    assert_equal false, resultado
+  end
 end
